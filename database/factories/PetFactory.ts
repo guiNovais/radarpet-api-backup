b834fee721a3aa4551335e504faf449b556607d4
Guilherme Novais <guilhermenovais.sh@gmail.com>
2023-07-18 17:00:04 -0300

    :technologist: Factories com enums aleatórios

--- database/factories/PetFactory.ts
+++ database/factories/PetFactory.ts
@@ -4,13 +4,14 @@ import { Situacao } from 'App/Models/Situacao'
 import { DateTime } from 'luxon'
 import { Especie } from 'App/Models/Especie'
 import { Cor } from 'App/Models/Cor'
+import { sample } from 'lodash'
 
 export default Factory.define(Pet, ({ faker }) => {
   return {
     nome: faker.person.firstName(),
-    especie: Especie.Cachorro,
-    cor: Cor.Amarelo,
-    situacao: Situacao.Perdido,
+    especie: sample(Especie),
+    cor: sample(Cor),
+    situacao: sample(Situacao),
     comentario: faker.lorem.paragraph(),
     vistoAs: DateTime.now().startOf('second'),
   }
