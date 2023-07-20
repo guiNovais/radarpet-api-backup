75a52dacc01549251ad5ff154d12b0d4ec0020e1
Guilherme Novais <guilhermenovais.sh@gmail.com>
2023-07-20 11:39:03 -0300

    :sparkles: Modelo coordenadas

---
+++ database/factories/CoordenadaFactory.ts
@@ -0,0 +1,8 @@
+import Coordenadas from 'App/Models/Coordenadas'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+
+export default Factory.define(Coordenadas, ({ faker }) => {
+  return {
+    //
+  }
+}).build()
