add2f510a64528d982e4eef834d3faa037f6b2cd
Guilherme Novais <guilhermenovais.sh@gmail.com>
2023-07-21 21:30:43 -0300

    :sparkles: Modelo usuário

---
+++ database/factories/UsuarioFactory.ts
@@ -0,0 +1,8 @@
+import Usuario from 'App/Models/Usuario'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+
+export default Factory.define(Usuario, ({ faker }) => {
+  return {
+    //
+  }
+}).build()
